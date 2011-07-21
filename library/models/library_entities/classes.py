3b3cb860b0a206ab3b96ec03781023c4c3161bdc
Serghei Filippov <serghfilip@hotmail.com>
2011-07-20 22:16:28 -0400

    Updated Class, added Spells and Linked the other Models together. See #2

--- library/models/library_entities/classes.py
+++ library/models/library_entities/classes.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from lib.config.database import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
 from library.config.classes import CLASS_SKILL_TYPES
+from library.config.magic import MAGIC_SOURCES
+from library.config.unknown import ATTRIBUTES
 from library.models.abstract import AbstractLibraryModel
 from library.models.library_entities.abstract import AbstractLibraryEntity
 from library.models.library_entities.feats import Feat
@@ -14,6 +16,24 @@ class DnDClass(AbstractLibraryEntity):
 
     class Meta(AbstractLibraryEntity.Meta):
         verbose_name_plural = "DnD Classes"
+
+    class_abv = models.CharField(
+        max_length=STND_ID_CHAR_LIMIT,
+        blank=False)
+    save_progression = models.ForeignKey('SaveProgression')
+    hit_die = models.PositiveIntegerField(blank=False, null=False)
+    base_attack_bonus = models.ForeignKey('BABProgression')
+    skill_points = models.PositiveIntegerField(blank=False, null=False)
+    spell_stat = models.CharField(
+        max_length=STND_ID_CHAR_LIMIT,
+        choices=ATTRIBUTES,
+        blank=True)
+    spellcasting_type=models.CharField(
+        max_length=STND_ID_CHAR_LIMIT,
+        choices=MAGIC_SOURCES
+        blank=True)
+    # If you fail to follow prohibitions/rules what happens
+    ex_class = TextField(blank=True)
 
     # TODO: finish this
     # Proficiencies
@@ -57,7 +77,7 @@ class ClassSkill(AbstractLibraryModel):
     """
     dnd_class = models.ForeignKey(
         DnDClass,
-        related_field='class_abilities',
+        related_field='class_skills',
         blank=False)
     skill = models.ForeignKey(
         Feat,
@@ -68,3 +88,30 @@ class ClassSkill(AbstractLibraryModel):
         default='class',
         blank=False)
 
+class ClassSpellLevel(AbstractLibraryModel):
+    """
+    Class Spells of a certain type at a certain level
+    """
+    dnd_class = models.ForeignKey(
+        DnDClass,
+        related_field='class_spells',
+        blank=False)
+    # 0-9 + epic? TODO look over ALL possible spell levels
+    spell_level = models.IntegerField(
+        blank=False,
+        null=False)
+    # +N type at this spell level
+    spell_level_increase = models.IntegerField(
+        blank=False,
+        null=False)
+    type = dnd_class = models.ForeignKey(
+        'ClassSpellType',
+        related_field='class_spell_level',
+        blank=False)
+    level = models.PositiveIntegerField(blank=False, null=False)
+
+class ClassSpellType(AbstractLibraryEntity):
+    """
+    A type of spell,
+      e.g. spell/day, known spell
+    """
