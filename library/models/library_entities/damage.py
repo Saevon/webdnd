0210f0464a17d1107b29216d85a3f8d1a1d687ed
Serghei Filippov <serghfilip@hotmail.com>
2011-07-21 21:24:27 -0400

    Added Damage. Required for  #4

---
+++ library/models/library_entities/damage.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from lib.config.database import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
+from library.models.library_entities.abstract import AbstractLibraryEntity
+
+class DamageType(AbstractLibraryEntity):
+    """
+    A damage type
+    """
+    damage_group = models.ForeignKey(
+        'DamageGroup',
+        related_field='damage_types',
+        blank=False)
+
+class DamageGroup(AbstractLibraryEntity):
+    """
+    A damage group
+    """
+
