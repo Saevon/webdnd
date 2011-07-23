e4bd3b98ada2b33a707d4e5bcb81e0f9319b5dc6
Serghei Filippov <serghfilip@hotmail.com>
2011-07-23 17:25:57 -0400

    Added Some Translated Items. See #4

---
+++ library/models/library_entities/items/items.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from library.config import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
+from library.models.abstract import AbstractLibraryEntity
+from library.models.items.abstract import AbstractItem
+from library.models.combat.weapon_info import RangedWeaponInfo
+
+class Item(AbstractItem):
+    """
+    A Generic Item
+    """
+
+class RangedWeapon(AbstractItem):
+    ranged = models.ForeignKey(
+        RangedWeaponInfo,
+        blank=True,
+        null=True)
+
