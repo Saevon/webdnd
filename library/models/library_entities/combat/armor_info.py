cd7ed1d5984370c008fcf45971f6835a7f54866b
Serghei Filippov <serghfilip@hotmail.com>
2011-07-23 17:19:34 -0400

    added Combat related weapon and armor information. Precedes #4

---
+++ library/models/library_entities/combat/armor_info.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from library.config import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
+from library.models.abstract import AbstractLibraryModel
+from library.models.proficiencies import ProficiencyGroup
+
+class ArmorInfo(AbstractLibraryModel):
+    """
+    Stats needed for generic armor
+    """
+    proficiency_group = models.ForeignKey(
+        ProficiencyGroup,
+        related_name='armor_info',
+        blank=False)
+    armor_bonus = models.IntegerField(blank=False)
+    # store as change .: if negative == penalty
+    armor_check_penalty = models.IntegerField(blank=False)
+    max_dexterity = models.PositiveIntegerField(blank=False)
+    arcane_spell_fail = models.IntegerField(blank=False)
+
---
+++ library/models/library_entities/combat/weapon_info.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+from library.config import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
+from library.models.abstract import AbstractLibraryModel
+from library.models.combat.damage import DamageType
+from library.models.combat.proficiencies import ProficiencyGroup
+
+class AbstractWeaponInfo(AbstractLibraryModel):
+    """
+    The main Combat Stats needed for a weapon
+    """
+    class Meta(AbstractLibraryModel.Meta):
+        abstract = True
+
+    proficiency_group = models.ForeignKey(
+        ProficiencyGroup,
+        related_name='%(class)s',
+        blank=False)
+    weapon_class = #sword, bow, etc
+    damage = #
+    damage_type = models.ForeignKey(
+        DamageType,
+        related_name='%(class)s',
+        blank=False)
+
+    critical_range = models.PositiveIntegerField(
+        blank=False,
+        null=False)
+    critical_multiplier = models.PositiveIntegerField(
+        blank=False,
+        null=False)
+
+class MeleeWeaponInfo(AbstractWeaponInfo):
+    """
+    The main Combat Stats needed for a melee waepon
+    """
+    reach = models.PositiveIntegerField(
+        default=5,
+        blank=False,
+        null=False)
+    double_weapon = models.ForeignKey(
+        'self',
+        related_name='+',
+        blank=True,
+        null=True)
+
+class ThrownWeaponInfo(AbstractWeaponInfo):
+    """
+    The main Combat Stats needed to throw an Item
+    """
+    range_increment = models.PositiveIntegerField(
+        default=10,
+        blank=False,
+        null=False)
+
+class RangedWeaponInfo(AbstractWeaponInfo):
+    """
+    The Main Combat Stats needed to use a Ranged Weapon
+    """
+    range_increment = models.PositiveIntegerField(
+        blank=False,
+        null=False)
+    double_weapon = models.ForeignKey(
+        'self',
+        related_name='+',
+        blank=True,
+        null=True)
+    # TODO: Ammunition
+    # TODO: need DnDAction Time
+    #reload_time = 
