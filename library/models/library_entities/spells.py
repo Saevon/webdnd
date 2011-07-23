6264d92ffa11f90ba133ac11a4079793e89bcd10
Serghei Filippov <serghfilip@hotmail.com>
2011-07-23 17:10:17 -0400

    Updated spells, added Domain Spells. Precedes #2

--- library/models/library_entities/spells.py
+++ library/models/library_entities/spells.py
@@ -3,6 +3,7 @@ from django.db import models
 from lib.config.database import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
 from library.config.magic import SCHOOLS_OF_MAGIC
 from library.config.magic.spells import SPELL_RANGES
+from library.models.abstract import AbstractLibraryModel
 from library.models.library_entities.abstract import AbstractLibraryEntity
 from library.models.library_entities.classes import DnDClass
 from library.models.modifiers.modifiers import Modifier
@@ -14,11 +15,11 @@ class Spell(AbstractLibraryEntity):
     Spell
     """
     
-    school = models.CharField(
-        max_length=STND_ID_CHAR_LIMIT,
-        choices=SCHOOLS_OF_MAGIC,
+    school = models.ForeignKey(
+        'MagicSubSchool',
+        related_field='spells',
         blank=False)
-    sub_school = models.ForeignKey('SpellSubSchool', blank=True)
+    #TODO: put description of what this is
     descriptors = models.ManyToManyField(
         'SpellDescriptor',
         related_name="spells",
@@ -32,7 +33,7 @@ class Spell(AbstractLibraryEntity):
         Modifier,
         related_name="spells",
         blank=True)
-    range = models.CharField(blank=True, max_length=100)
+    #range = models.CharField(blank=True, max_length=100)
     
     # components
     verbal_component = models.BooleanField(default=False)
@@ -44,33 +45,73 @@ class Spell(AbstractLibraryEntity):
     
     casting_time = models.ForeignKey("ActionTimeDuration", blank=False)
     resistable = models.BooleanField(default=True)
+    negates = models.ForeignKey(
+        'self',
+        related_feld='negated_by',
+        blank=True,
+        null=True)
+    #upgraded spells? cure moderate -> serious -> critical
+
+class Domain(AbstractLibraryEntity):
+    """
+    A Domain
+    """
+    granted_power = models.ForeignKey(
+        Feat,
+        related_field='domains',
+        blank=False)
+
+class DomainSpellLevel(AbstractLibraryModel):
+    """
+    A domain Spell level
+    """
+    class Meta(AbstractLibraryModel.Meta):
+        unique_together = (
+            ('domain','level'),
+        )
+
+    domain = models.ForeignKey(
+        Domain,
+        related_field='spells',
+        blank=False)
+    level = models.PositiveIntegerField(null=False, blank=False)
+    spell = models.ForeignKey(
+        Spell,
+        related_field='domain_spells',
+        blank=False)
 
 class SpellDescriptor(AbstractLibraryEntity):
     """
     A spell descriptor.
     """
 
-class CastingLevelClassPair(AbstractLibraryEntity):
+class CastingLevelClassPair(AbstractLibraryModel):
     """
     A pair of a class and a casting level.
     """
-    
-    dnd_class = models.ForeignKey("DnDClass", blank=False)
+    class Meta(AbstractLibraryModel.Meta):
+        unique_together = (
+            ('dnd_class', 'spell'),   
+        )
+
+    dnd_class = models.ForeignKey(
+        DnDClass,
+        related_field='spell_list',
+        blank=False)
+    #Or is it? related to class.py spell/day class
     casting_level = models.PositiveSmallIntegerField(blank=False)
+    spell = models.ForeignKey(
+        Spell,
+        related_field='class_spell_list',
+        blank=False)
 
-class SpellSubSchool(AbstractLibraryEntity):
+class MagicSubSchool(AbstractLibraryEntity):
     """
-    A spell sub-school.
+    A sub school of Magic
     """
-
-class SpellRange(AbstractLibraryEntity):
-    """
-    A range of a spell. FFUUUUUUU!
-    """
-    
-    range_type = models.CharField(
+    #TODO: title could duplicate across Schools of Magic...
+    school = models.CharField(
         max_length=STND_ID_CHAR_LIMIT,
-        choices=SPELL_RANGES,
-        blank=True)
-    
+        choices=SCHOOLS_OF_MAGIC,
+        blank=False)
 
