e4bf273ba0a04c4b39db3cd3cb8d3e51872592dc
Serghei Filippov <serghfilip@hotmail.com>
2011-07-23 23:17:35 -0400

    Added Shared Proficiencies. See #4

--- library/models/library_entities/combat/proficiencies.py
+++ library/models/library_entities/combat/proficiencies.py
@@ -2,15 +2,29 @@ from django.db import models
 
 from library.config import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
 from library.config.items.proficiencies import PROFICIENCY_TYPES
-from library.models.abstract import AbstractLibraryEntity
+from library.models.abstract import AbstractLibraryEntity, AbstractLibraryModel
 
 class ProficiencyGroup(AbstractLibraryEntity):
     """
     A Proficiency:
         e.g. simple, martial etc.
     """
-    type = CharField(
+    type = models.CharField(
         max_length=STND_ID_CHAR_LIMIT,
         choices=PROFICIENCY_TYPES,
+        blank=False)
+
+class SharedProficiency(AbstractLibraryModel):
+    """
+    A Shared Proficiency
+    """
+    # main == the one you put on the Char Sheet
+    main = models.ForeignKey(
+        AbstractItem,#Check if you can point to an abstract?
+        related_field='shared_proficiencies_main',
+        blank=False)
+    other = models.ForeignKey(
+        AbstractItem,#Check if you can point to an abstract?
+        related_field='shared_proficiencies_other',
         blank=False)
 
