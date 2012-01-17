9152fb89de0875945b44cfbf76c41c821a66b47b
Serghei Filippov <blastowind@gmail.com>
2012-01-16 19:44:01 -0500

    Created a Race and Creature. See #7

--- library/models/library_entities/creatures.py
+++ library/models/library_entities/creatures.py
@@ -1,9 +1,57 @@
 from django.db import models
 
 from lib.config.database import STND_CHAR_LIMIT, STND_ID_CHAR_LIMIT
+from library.config.creatures import SIZES
 from library.models.library_entities.abstract import AbstractLibraryEntity
 from library.models.library_entities.abilities import Ability
 from library.models.library_entities.classes import SaveProgression
+
+class Creature(AbstractLibraryEntity):
+    """
+    A base Creature
+    """
+
+    abilities = models.ManyToManyField(
+        Ability,
+        related_name="creature_types",
+        blank=True,
+        null=True)
+    # TODO: spells gotten from the class
+
+class Race(AbstractLibraryEntity):
+    """
+    A Race one can Play
+    """
+    size = models.CharField(
+        max_length=STND_ID_CHAR_LIMIT,
+        choices=SIZES,
+        blank=False)
+    level_adjustment = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False)
+    # One time changes/effects that occur foer being of this Race
+    modifiers = models.ManyToManyField(
+        Modifier,
+        related_name="races",
+        blank=True,
+        null=True)
+    # Changes that are lost during polymorph "continuous" abilities
+    abilities = models.ManyToManyField(
+        Ability,
+        related_name="races",
+        blank=True,
+        null=True)
+    # TODO: Restrictions? e.g. animal = Neutral Alignment
+    # TODO: spells gotten from the class
+    favored_class = models.CharField(
+        max_length=STND_CHAR_LIMIT,
+        blank=True)
+    languages = models.ManyToManyField(
+        Language,
+        related_name="races",
+        blank=True,
+        null=True)
 
 class CreatureType(AbstractLibraryEntity):
     """
@@ -13,7 +61,7 @@ class CreatureType(AbstractLibraryEntity):
     base_attack_bonus = models.DecimalField(max_digits=5, decimal_places=2)
     saves = models.ForeignKey(
         SaveProgression,
-        related_name="creatures")
+        related_name="creature_types")
     skill_points = models.PositiveIntegerField(blank=False, null=False)
     abilities = models.ManyToManyField(
         Ability,
@@ -34,4 +82,3 @@ class CreatureSubType(AbstractLibraryEntity):
         null=True)
     # TODO: Restrictions? e.g. animal = Neutral Alignment
     # TODO: Stat Changes?
-
