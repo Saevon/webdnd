67bdb900c1f9721b0a6daa8f972554396a35365f
Serghei Filippov <blastowind@gmail.com>
2012-01-21 16:46:24 -0500

    removed model import, as many have been changed. See #5

--- library/models/__init__.py
+++ library/models/__init__.py
@@ -1,35 +1,7 @@
 """
 Tables relating the the Data Stored in the Library
 """
-from library.models.accounts import LibraryAccount
-from library.models.sources import Source
-from library.models.library_entities.conditions import Condition
-from library.models.library_entities.abilities import Ability, AbilityType
-from library.models.library_entities.references import Article, Example, Rule, Term
-from library.models.library_entities.skills import Skill, SkillSample
-from library.models.library_entities.spells import CastingLevelClassPair, Spell, SpellDescriptor
-from library.models.library_entities.classes import DnDClass
-from library.models.modifiers.modifiers import Modifier
-from library.models.modifiers.saving_throws import SavingThrow
-from library.models.units import ActionTimeDuration
 
 __all__ = [
-    ActionTimeDuration,
-    Article,
-    CastingLevelClassPair,
-    Condition,
-    DnDClass,
-    Example,
-    Ability,
-    AbilityType,
-    LibraryAccount,
-    Modifier,
-    Rule,
-    SavingThrow,
-    Skill,
-    SkillSample,
-    Source,
-    Spell,
-    SpellDescriptor,
-    Term,
+
 ]
