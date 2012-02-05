441e502e2b4f973a0162205f47aba62153e12423
Serghei Filippov <blastowind@gmail.com>
2012-02-05 08:34:05 +0400

    Another renaming change. See #36

--- settings.py
+++ settings.py
@@ -115,6 +115,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    # 'webdnd.game',
-    'webdnd.library',
+    # 'webdnd.player',
+    'webdnd.dnd',
 )
