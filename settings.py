a4180947b60923f683f48a3513fe311dc2eb7188
Serghei Filippov <blastowind@gmail.com>
2012-09-29 02:37:13 -0400

    Disabled Pretty printing middleware, it causes problems right now.  * Need to look in and fix/replace that

--- settings.py
+++ settings.py
@@ -21,7 +21,11 @@ DEV_MODE = True
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 COMPRESS_ENABLED = not DEBUG
-PRETTIFY_HTML = DEBUG
+
+# This cleans up the HTML, it should not be doing that...
+# So its disabled for now
+# We need it to NOT autoescape stuff in <script> tags
+PRETTIFY_HTML = False#DEBUG
 
 
 
