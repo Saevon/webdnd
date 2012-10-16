35dfe05ad1eb83009c8a60f4f9b219c628408dc3
Serghei Filippov <blastowind@gmail.com>
2012-10-16 19:43:49 -0400

    Removed reference to SyncraeApi

--- player/urls/api.py
+++ player/urls/api.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
 from webdnd.player.views.account import UserSearchApi
-from webdnd.player.views.account import UserDetailsApi
 
 urlpatterns = patterns('webdnd.player.views',
     # Account
     url(r'^account/search/(?P<text>.*)/?$', UserSearchApi.as_view(), name="account_api_search"),
-    url(r'^account/user/self/?$', UserDetailsApi.as_view(), name="account_api_user_self"),
 )
