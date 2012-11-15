d5ce8989ebf3d1526fb004d2201e2690bbcfebd8
Serghei Filippov <blastowind@gmail.com>
2012-11-15 17:00:37 -0500

    Fixed up model import

--- player/models/__init__.py
+++ player/models/__init__.py
@@ -3,16 +3,3 @@ from webdnd.player.models.campaigns import *
 from webdnd.player.models.characters import *
 from webdnd.player.models.alignments import *
 
-
-__all__ = [
-    User,
-    Preference,
-    UserPreference,
-
-    Campaign,
-    CampaignPreference,
-    Player,
-
-    Character,
-    Alignment,
-]
